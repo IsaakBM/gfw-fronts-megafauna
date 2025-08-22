46973978dab2cfb39d62816e8f0fbf13739949b0
IsaakBM <ibrito@conservation.org>
2025-08-22 09:41:56 -0700

    Add test code

--- tests/Untitled.R
+++ tests/Untitled.R
@@ -12,3 +12,10 @@ gwf_files <- list.files(
 gfw_yr_ls <- vector("list", length = length(gwf_files))
 for(i in seq_along(gfw_yr_ls)) {
   gfw_yr_ls[[i]] <- readRDS(gwf_files[i])}
+
+
+wsT <- readRDS("data-raw/mm_mmf_02/mmf_WhaleShark_2011-08_71214.rds")
+sbT <- readRDS("data-raw/NosyVe_02/NosyVe_2018-05_EA583867.rds")
+trtT <- readRDS("data-raw/TMNT_02v02/TMNT_2019-01_657110.rds")
+
+unique(wsT$ptt)
