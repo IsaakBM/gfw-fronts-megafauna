bae90037fdbf955af6a21295ea6e61ecb75717f7
IsaakBM <ibrito@conservation.org>
2025-09-02 10:58:54 -0700

    Update 03_run_make_front_species_plot.R

--- scripts/03_run_make_front_species_plot.R
+++ scripts/03_run_make_front_species_plot.R
@@ -53,3 +53,60 @@ test03 <- make_front_species_plot(
   by_species = FALSE
 )
 ggsave("outputs/figures/final/all-gears_all-fronts_figv04.png", plot = test03, width = 10, height = 10, dpi = 400)
+
+
+
+
+
+# Define all gear types
+gear_list <- c(
+  "drifting_longlines","tuna_purse_seines","fishing","trawlers","dredge_fishing",
+  "set_longlines","pole_and_line","squid_jigger","fixed_gear","set_gillnets",
+  "other_purse_seines","purse_seines"
+)
+
+# Preallocate a named list
+test_plot <- setNames(vector("list", length(gear_list)), gear_list)
+for (i in seq_along(gear_list)) {
+  g <- gear_list[i]
+  message("Processing gear: ", g)
+  
+  test_plot[[g]] <- make_front_species_plot(
+    tracks_dir = NULL,
+    grid_file  = "data-raw/agg_cell_gear_mzc_rob.rds",
+    gears      = g,
+    grid_size  = 0.10,
+    xlim       = c(30, 65),
+    ylim       = c(-35, -5),
+    # output_dir = "outputs/gear_plots",
+    width      = 10, height = 5, dpi = 300,
+    by_species = FALSE
+  )
+}
+# 1) One shared scale with fixed limits + identical name
+effort_scale <- scale_fill_distiller(
+  palette = "GnBu",
+  direction = 1,
+  trans = "log10",
+  breaks = c(1, 10, 100, 1e3, 1e4, 1e5),
+  limits = c(1, 1e5),  # <-- FORCE identical range across all children
+  labels = scales::label_number(accuracy = 1, big.mark = ","),
+  na.value = "white",
+  name = "Fishing Effort<br/>by 10 km<sup>2</sup> (Log<sub>10</sub> Scale)",  # same name in all
+  guide = guide_colorbar(title.position = "top")
+)
+
+# 2) Apply the SAME scale to each plot (and don't hide the fill guide anywhere)
+test_plot_fixed <- lapply(test_plot, function(p) {
+  p + effort_scale + guides(fill = guide_colorbar()) +
+    theme(legend.position = "right")  # temp position; will be collected later
+})
+
+# 3) Wrap and collect
+patch_all_gt <- wrap_plots(plotlist = test_plot_fixed, ncol = 4, byrow = TRUE) +
+  plot_layout(guides = "collect") +
+  plot_annotation(tag_prefix = "(",
+                  tag_levels = "a", 
+                  tag_suffix = ")") &
+  theme(legend.position = "bottom", legend.box = "vertical")
+ggsave("outputs/asdf_v03.png", plot = patch_all_gt, width = 40, height = 40, dpi = 300)
